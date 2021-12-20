b400df4f89d1442cf5886d1129618945cbad7a7c
Hadar Ben Ishay <49114789+hadardev@users.noreply.github.com>
2021-12-20 19:20:42 +0200

    Create Nav.style.js

---
+++ src/components/Nav/inline-assets/style/Nav.style.js
@@ -0,0 +1,24 @@
+import { css } from '@emotion/react';
+
+export const NavContainerStyle = css`
+    display: flex;
+    justify-content: center;
+    top: 0;
+    width: 100%;
+    position: fixed;
+    padding-top: 8px;
+    transition: all 0.5s ease 0s;
+    background-color: #EDB596;
+    overflow: hidden;
+    box-shadow: 0 4px 8px 0 rgb(0 0 0 / 20%), 0 6px 20px 0 rgb(0 0 0 / 19%);
+    z-index: 5;
+    margin: 0 auto;
+`;
+
+export const logoStyle = css`
+    width: 43px;
+    height: 43px;
+    border-radius: 50%;
+`;
+
+
