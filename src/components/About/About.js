c0ed74e040d11188eb81830eaba06a86a6930468
Hadar Ben Ishay <49114789+hadardev@users.noreply.github.com>
2021-12-20 19:15:10 +0200

    Create About.js

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+/** @jsxImportSource @emotion/react */
+
+import { aboutContainerStyle } from "./inline-assets/style/About.style";
+
+const About = () => {
+    return (
+        <div css={aboutContainerStyle}>
+            <p>היי, אני עידן, נטורופת מוסמך, רפלקסולוג בכיר, מטפל בצמחי מרפא, ארומתרפיה, פרחי באך, רייקי ועוד. </p>
+            <p>כיום אני מטפל באופן פרטי, בנוסף בשבע השנים האחרונות אני חלק מצוות ההוראה של מכללת רידמן כאסיסטנט ומתרגל במגמת הרפלקסולוגיה ובונה פורמולות צמחים ושמנים אתריים בהתאמה אישית.</p>
+        </div>
+    );
+}
+
+export default About;
