ec0f9fb557212e4e76e09b03fb070c186780de95
Hadar Ben Ishay <49114789+hadardev@users.noreply.github.com>
2021-12-20 19:08:39 +0200

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import './../style/App.css';
+import Homepage from './Homepage/Homepage';
+import Nav from './Nav/Nav';
+
+function App() {
+  return (
+    <div className="App">
+        <Nav />
+        <Homepage />
+    </div>
+  );
+}
+
+export default App;
