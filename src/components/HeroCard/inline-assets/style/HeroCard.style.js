0019fd13e510766c6a4fd0c085108061434ed5f0
Hadar Ben Ishay <49114789+hadardev@users.noreply.github.com>
2021-12-20 19:19:30 +0200

    Create HeroCard.style.js

---
+++ src/components/HeroCard/inline-assets/style/HeroCard.style.js
@@ -0,0 +1,25 @@
+import { css } from '@emotion/react';
+
+export const heroCardStyle = css`
+    min-height: 420px;
+    text-align: center;
+`;
+
+export const heroCardImageStyle = css`
+    position: absolute;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 100%;
+    z-index: -1;
+    opacity: 0.5;
+    height: 400px;
+`;
+
+export const contentStyle = css`
+    padding: 0 1em;
+`;
+
+export const titleStyle = css`
+    padding-top: 100px;
+`;
