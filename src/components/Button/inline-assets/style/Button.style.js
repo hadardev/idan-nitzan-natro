46e534559aa152b05aa4ffce006d6b09fa1d2de9
Hadar Ben Ishay <49114789+hadardev@users.noreply.github.com>
2021-12-20 19:16:46 +0200

    Create Button.style.js

---
+++ src/components/Button/inline-assets/style/Button.style.js
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+
+export const ButtonStyle = css`
+    display: inline-block;
+    background-color: #D12323;
+    text-decoration: none;
+    color: white;
+    padding: 1em;
+    margin: 0.5em 0;
+    border-radius: 3%;
+`;
